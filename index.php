1507ce2524b6c1831db4a31397224190a2622447
Conrad Zelck <git@simpel.cc>
2024-03-05 22:07:54 +0100

    initial commit
    Signed-off-by: Conrad Zelck <git@simpel.cc>

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- This php line should be removed on your personal webiste -->
+  <?php include dirname($_SERVER['DOCUMENT_ROOT']) . "/simpel.cc/php/head.php"; ?>
+  <!-- Those 3 lines should be uncommented on your personal website -->
+  <!-- <meta charset="utf-8"> -->
+  <!-- <title>Perfect Website</title> -->
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+  <link rel="stylesheet" href="perfect.style.css">
+  <!-- style.css overwrites some of the perfect.style.css because of header -->
+  <!-- style.css should be removed on your personal website -->
+  <link rel="stylesheet" href="style.min.css">
+</head>
+
+<body>
+  <!-- header should be removed on your personal website -->
+  <header>
+    <?php include dirname($_SERVER['DOCUMENT_ROOT']) . "/simpel.cc/php/header.php"; ?>
+  </header>
+
+  <main>
+    <!-- This <h1> should be uncommented on your personal website -->
+    <!-- <h1>This is the <em>final</em> website.</h1> -->
+
+    <p>And it’s <em>really</em> more perfect than the last guy’s.</p>
+
+    <h2>Minimal things are needed to make this perfect.</h2>
+
+    <p>
+      <strong>Fourten declarations, a <code class="language-plaintext highlighter-rouge">@media</code> block and one attribute.</strong><br>
+      <a href="perfect.style.css">Watch this CSS file</a>.
+    </p>
+
+    <p>That’s how much CSS and HTML it took to turn that <a href="https://motherfuckingwebsite.com/" target="_blank">inspiration</a> into this finally perfect masterpiece. It’s so simple and it still has all the glory of the original perfect website:
+    </p>
+
+    <ul>
+      <li><em>Finally</em> lightweight and loads fast;</li>
+      <li><em>Finally</em> fits on all your screens;</li>
+      <li><em>Finally</em> looks the same in all your browsers;</li>
+      <li><em>Won’t</em> bleach your eyes if you like dark things;</li>
+      <li><em>Still</em> accessible to every one that visits your site;</li>
+      <li><em>Still</em> legible.</li>
+    </ul>
+
+    <h3>And guess what:</h3>
+
+    <p>You never knew it, but it’s easy to improve reliability on your site. Here’s how.</p>
+
+    <h2>Little more accessibility</h2>
+
+    <p>I know you love marginalization, but you should take care of people.
+      The <abbr title="HyperText Markup Language">HTML</abbr> tag doesn’t include the <a href="https://www.w3.org/TR/html5/dom.html#the-lang-and-xml:lang-attributes" target="_blank"><code>lang</code> attribute</a> in your webpage.<br>
+      You ever listen to a wrong language screen reader lecture</a>? What a bad experience.</p>
+
+    <h2>Typography matters</h2>
+
+    <p><a href="https://smartquotesforsmartpeople.com/" target="_blank">Don’t be dumb with typography</a>. Don’t let improper marks and quotes make their way onto websites because of dumb defaults in applications and <abbr title="Conten Management System">CMSs</abbr>.<br>
+      And did you let the browser choose the font? Don’t make me read such text with <em>Times New Roman</em>.</p>
+
+    <h2>License</h2>
+
+    <p>Do you revendicate intellectual property? No. So you must <a href="https://choosealicense.com/" target="_blank">choose a public license</a>.<br>
+      And as you’ll never be perfect, let others <a href="https://github.com/LeoColomb/perfectmotherfuckingwebsite" target="_blank">improve your site</a> or <a href="https://github.com/LeoColomb/perfectmotherfuckingwebsite/blob/master/LICENSE"
+        target="_blank">do what they want to</a>.</p>
+
+    <h2>This is the last satire</h2>
+
+    <p>I’m impressed about what the creator of <a href="https://perfectmotherfuckingwebsite.com" target="_blank">this site’s inspiration did</a>. What I’m saying is that it’s so, so simple to make sites easier to read. Websites are broken by default
+      — they are functional, high-performing, and accessible, but they’re also ugly. You and all the other web designers out there need to make them not total shit.</p>
+
+    <blockquote>
+      <p>“Don’t ignore stupid things or you will stay at the motherfucker level.”
+        — Brad Hollande</p>
+    </blockquote>
+
+    <hr />
+
+    <h2>Epilogue</h2>
+
+    <p>Inspired by the geniuses behind <a href="https://motherfuckingwebsite.com" target="_blank">motherfuckingwebsite.com</a>, <a href="http://bettermotherfuckingwebsite.com" target="_blank">bettermotherfuckingwebsite.com</a> and <a
+        href="https://perfectmotherfuckingwebsite.com" target="_blank">perfectmotherfuckingwebsite.com</a>.</p>
+
+    <p>This page was licensed under <a href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">CC0</a>.</p>
+  </main>
+
+</body>
+
+</html>
