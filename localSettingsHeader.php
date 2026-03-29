77ef805f6bb7f1893991e73f7e03f1faa5075a99
Conrad Zelck <git@simpel.cc>
2026-03-29 12:53:12 +0200

    fix: use correct licence in local settings
    MIT was copied from the template. But it is CC0.
    
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- localSettingsHeader.php
+++ localSettingsHeader.php
@@ -1,6 +1,6 @@
 <?php
   define("SITE_TITLE", "Perfect Website");
-  define("SITE_COPYRIGHT_CONTENT", "MIT 2020 Simpel");
+  define("SITE_COPYRIGHT_CONTENT", "CC0 2020 Simpel");
   define("SITE_H1", "Die <em>endgültige</em> Webseite.");
   define("SITE_GITHUB", "https://github.com/SimpelMe");
 ?>
